8226bec17e82b537168ae6741bcaf31c2d187fba
Kevin Nielsen <knielsen24@gmail.com>
2022-04-27 12:26:00 -0700

    commit tree cf5f681ae087cbc5b89b389702fd41381e4e516f

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const surprise = ["surprise", "birthday", "baby shower"]
+
+
+const writeCards = (names, event) => {
+    const thankYouNote = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouNote.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouNote;
+}
+
+console.log(writeCards(names, surprise[0]));
+
+const countDown = () => {
+    let x = 11;
+        while (x <= 11 && x > 0) {
+            x--;
+            console.log(x);
+        }
+}
+
+countDown();
